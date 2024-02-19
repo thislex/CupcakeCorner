d7448c2c8644734304afbec06a5e9d16cda19fb4
Lexter T <lexter.tapawan@gmail.com>
2024-02-19 12:31:42 +0000

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Lexter Tapawan on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
