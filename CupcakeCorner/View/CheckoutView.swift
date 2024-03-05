1cdd7330565ef8b68d64ea14ff99bdca3b18321e
Lexter T <lexter.tapawan@gmail.com>
2024-03-05 08:08:39 +0000

    Codables implemented. Validating and disabling forms.

---
+++ CupcakeCorner/View/CheckoutView.swift
@@ -0,0 +1,41 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Lexter Tapawan on 20/02/2024.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    var order: Order
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place Order", action: { })
+                    .padding()
+            }
+        }
+        .navigationTitle("Check Out")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
